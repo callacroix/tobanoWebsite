afcdbc3ad7cddaf4451169a04c99d75a268c9d0a
Carine Lacroix <callacroix@gmail.com>
2018-10-27 21:33:26 +0200

    remove file

--- src/app/welcome/download.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'section-download',
-  templateUrl: 'download.component.html',
-  styleUrls: ['download.component.css']
-})
-export class DownloadComponent {
-}
