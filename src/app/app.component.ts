320ab43eae406926f0f49299707adc797573512e
Carine Lacroix <callacroix@gmail.com>
2018-10-28 13:48:37 +0100

    move the section from the index to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Tobano';
+export class AppComponent implements OnInit {
+  isLoading = false;
+
+  ngOnInit() {
+    // TODO
+
+    this.isLoading = true;
+  }
 }
